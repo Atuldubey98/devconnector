b183eccf249e826f69eb3b55917f47dd5f551776
Atuldubey98 <32754884+Atuldubey98@users.noreply.github.com>
2020-09-23 15:26:44 +0530

    initial routes

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports ={
+    MONGO_URI : "mongodb+srv://atuldubey:08091959@cluster0.isxbl.mongodb.net/DevConnecter?retryWrites=true&w=majority"
+}
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const users = require("./routes/api/users");
+const posts = require("./routes/api/posts");
+const profile = require("./routes/api/profile");
+const port = process.env.PORT || 9000
+const app = express()
+// DB config
+
+const db = require("./config/keys").MONGO_URI
+
+// Connect to mongofb
+
+mongoose.connect(db,{
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+}).then(()=>{
+    console.log("DB Connected");
+}).catch(err=>{
+    console.log(err);
+})
+
+app.get("/", (req, res)=>{
+    res.send("Hello")
+})
+
+app.use("/api/users", users)
+app.use("/api/posts", posts)
+app.use("/api/profile", profile)
+
+
+app.listen(port , ()=>{
+    console.log(`Server is Listening on ${port}`);
+})
