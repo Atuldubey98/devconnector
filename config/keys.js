aa34c633d4e9e7d9197633b2f549155e2ac78016
Atuldubey98 <32754884+Atuldubey98@users.noreply.github.com>
2020-09-23 17:39:52 +0530

    Second Commit

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports ={
-    MONGO_URI : "mongodb+srv://atuldubey:08091959@cluster0.isxbl.mongodb.net/DevConnecter?retryWrites=true&w=majority"
+    MONGO_URI : "mongodb+srv://atuldubey:08091959@cluster0.isxbl.mongodb.net/DevConnecter?retryWrites=true&w=majority",
+    SECRET_KEY: "Ramatul98#"
 }
--- server.js
+++ server.js
@@ -3,10 +3,15 @@ const mongoose = require("mongoose")
 const users = require("./routes/api/users");
 const posts = require("./routes/api/posts");
 const profile = require("./routes/api/profile");
+const bodyParser = require("body-parser")
+const passport = require("passport")
 const port = process.env.PORT || 9000
 const app = express()
 // DB config
-
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+app.use(bodyParser.json())
 const db = require("./config/keys").MONGO_URI
 
 // Connect to mongofb
@@ -20,9 +25,10 @@ mongoose.connect(db,{
     console.log(err);
 })
 
-app.get("/", (req, res)=>{
-    res.send("Hello")
-})
+// Passport middleware
+app.use(passport.initialize())
+// Passport Config
+// Use Routes
 
 app.use("/api/users", users)
 app.use("/api/posts", posts)
